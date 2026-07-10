74ec06ddf8b35a053beb297926a783d234ebf5d6
Abdul Rehman <abdulrehmanafridi88@gmail.com>
2026-07-10 21:08:10 +0500

    Add user authentication features with MongoDB integration
    - Updated package.json and package-lock.json to include bcryptjs, mongoose, and nodemailer dependencies.
    - Created database connection utility in dbConfig.ts for MongoDB.
    - Implemented user model in userModel.js with validation.
    - Added login and signup page components.

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+    try {
+        const uri = process.env.MONGOOSE_URI;
+        if (!uri) {
+            throw new Error("MONGOOSE_URI is not defined in the environment variables.");
+        }
+        const db = mongoose.connection;
+        db.on("connected", () => {
+            console.log("Connected to MongoDB");
+        });
+        db.on("error", (error) => {
+            console.error("Error connecting to MongoDB:", error);
+            
+            process.exit(1);
+        });
+        await mongoose.connect(uri);
+        
+    }
+    catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+
+
